6f75d7c0cdd323e99b8ef23c8a632fd3fac23d0a
Keith <hello@keithdowd.com>
2017-02-20 16:43:32 -0500

    Added comments

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -8,10 +8,13 @@
 
     /*
      * AngularJS controller
-     * @param {servivce} $scope - AngularJS scope service
+     * @param {service} $scope - AngularJS scope service
      */
     function lunchCheckController($scope) {
+        // Message to user indicating whether there are too many food items or not
         $scope.countFoodItems = "";
+
+        // Function to check for too many food items
         $scope.isTooManyFoodItems = isTooManyFoodItems;
 
         /*
@@ -19,12 +22,15 @@
          * @param {string} foodInput - Comma-separated string of food items 
          */
         function isTooManyFoodItems(foodInput) {
+
             if (!isNotUndefined(foodInput) || !isNotBlank(foodInput)) {
+                // Indicate to user to enter data if none is present
                 $scope.countFoodItems = 'Please enter data first';
             } else {
-                var foodItems = foodInput.split(',');
-                foodItems = foodItems.filter(isNotBlank);
-                $scope.countFoodItems = (foodItems.length <= 3) ? 'Enjoy!' : 'Too much!';
+                // Determine how many food items are present and return appropriate message to user
+                var foodItems = foodInput.split(','); // Split on commas
+                foodItems = foodItems.filter(isNotBlank); // Remove empty items
+                $scope.countFoodItems = (foodItems.length <= 3) ? 'Enjoy!' : 'Too much!'; // Return appropriate message based on count
             }
         }
 
@@ -32,7 +38,7 @@
          * Checks whether a string is blank
          * @param {string} value - A string to check if blank
          * @return {boolean} - A boolean indicating whether value is not blank (true) or is blank (false)
-         */ 
+         */
         function isNotBlank(value) {
             return value.trim() !== '';
         }
@@ -41,7 +47,7 @@
          * Checks whether a string is undefined
          * @param {string} value - A string to check if undefined
          * @return {boolean} - A boolean indicating whether value is not undefined (true) or is undefined (false)
-         */ 
+         */
         function isNotUndefined(value) {
             return value !== undefined;
         }
